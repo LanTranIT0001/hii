2fd6d32a0acf5ce6e86978e4197e03f8e96a5630
Lan Tran Thi Ngoc <lantran.31241027467@st.ueh.edu.vn>
2026-04-16 06:09:35 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+session_start();
+
+$config = require __DIR__ . '/../config/app.php';
+
+spl_autoload_register(static function (string $class): void {
+    $prefixes = [
+        'Core\\' => __DIR__ . '/../core/',
+        'App\\Controllers\\' => __DIR__ . '/../app/controllers/',
+        'App\\Models\\' => __DIR__ . '/../app/models/',
+    ];
+
+    foreach ($prefixes as $prefix => $baseDir) {
+        if (strpos($class, $prefix) !== 0) {
+            continue;
+        }
+
+        $relativeClass = substr($class, strlen($prefix));
+        $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
+        if (is_file($file)) {
+            require $file;
+        }
+    }
+});
+
+$route = isset($_GET['r']) ? (string) $_GET['r'] : 'auth/login';
+
+$router = new Core\Router($config);
+$router->dispatch($route);
